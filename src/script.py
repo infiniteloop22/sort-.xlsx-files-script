0ecdb847645fd6c25729fabd6882335c33e02f82
infiniteloop22 <infiniteloop22@protonmail.com>
2025-08-16 16:11:17 -0400

    Updated src

--- src/script.py
+++ src/script.py
@@ -3,20 +3,20 @@ import pandas as pd
 def read_excel_file(file_path):
     return pd.read_excel(file_path)
 
-def get_columns(data):
-    return data.columns.to_list()
+def get_columns(dataframe):
+    return dataframe.columns.to_list()
 
-def sort_data(data, columns):
-    return data.sort_values(by=columns)
+def sort_data(dataframe, columns):
+    return dataframe.sort_values(by=columns)
 
 def write_excel_file(file_path, sorted_data):
     sorted_data.to_excel(file_path, index=False)
     print(f"\nWritten to {file_path}..")
 
 def process_data(input_file, output_file):
-    data = read_excel_file(input_file)
-    columns = get_columns(data)
-    sorted_data = sort_data(data, columns)
+    df = read_excel_file(input_file)
+    columns = get_columns(df)
+    sorted_data = sort_data(df, columns)
     write_excel_file(output_file, sorted_data)
 
 def main():
